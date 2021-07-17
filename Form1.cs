351f9d0fa6819739a3aec47e45a616665302cf11
ae <ae@ACER>
2021-07-17 16:38:50 +0800

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace problem1_3.calapini_bscpe1_5
+{
+    public partial class Form1 : Form
+    {
+        int a = 0, b = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            a = a + 1;
+            button2.Enabled = true;
+            button1.Enabled = false;
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            b = b + 1;
+            button2.Enabled = false;
+            button1.Enabled = true;
+        }
+    }
+}
